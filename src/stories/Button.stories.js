5142be3577a9003189d2278c94675b991c2bda65
Anna Musial <ania.musial13@gmail.com>
2022-02-17 10:29:04 +0100

    added storybook

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,32 @@
+import SfButton from '../system/components/SfButton.vue';
+
+export default {
+  title: 'COMPONENTS/Button',
+  component: SfButton,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    onClick: {},
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+    },
+  },
+};
+
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { SfButton },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: '<sf-button v-bind="args" />',
+});
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
